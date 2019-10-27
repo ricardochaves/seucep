7a0ec4c291529fcb3622354ee4a25bbbd3d4b114
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2019-10-27 20:28:36 -0300

    added more tests

--- seucep/apps/core/models.py
+++ seucep/apps/core/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Addresses(models.Model):
 
     cep = models.CharField(primary_key=True, max_length=8, verbose_name="CEP")
-
     addresses = models.CharField(max_length=36, verbose_name="Logradouro")
     state = models.CharField(max_length=2, verbose_name="State")
     complement = models.CharField(blank=True, null=True, max_length=100, verbose_name="Complement")
---
+++ tests/test_renders.py
@@ -0,0 +1,49 @@
+from django.test import Client
+from django.test import TestCase
+from django.test import override_settings
+from seucep.apps.core.models import Addresses
+
+
+@override_settings(STATICFILES_STORAGE="django.contrib.staticfiles.storage.StaticFilesStorage")
+class TestRoutes(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        Addresses.objects.create(
+            cep="27185000",
+            addresses="Arrozal (Piraí)",
+            state="RJ",
+            # complement = "",
+            # neighborhood = "",
+            # city = "",
+        )
+
+    def test_should_return_yaml_result(self):
+
+        headers = {"HTTP_ACCEPT": "application/yaml"}
+
+        response = self.client.get("/api/v1/ceps/27185000/", None, None, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response._headers["content-type"][0], "Content-Type")
+        self.assertEqual(response._headers["content-type"][1], "application/yaml; charset=utf-8")
+
+    def test_should_return_xml_result(self):
+
+        headers = {"HTTP_ACCEPT": "application/xml"}
+
+        response = self.client.get("/api/v1/ceps/27185000/", None, None, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response._headers["content-type"][0], "Content-Type")
+        self.assertEqual(response._headers["content-type"][1], "application/xml; charset=utf-8")
+
+    def test_should_return_piped_result(self):
+
+        headers = {"HTTP_ACCEPT": "text/piped"}
+
+        response = self.client.get("/api/v1/ceps/27185000/", None, None, **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response._headers["content-type"][0], "Content-Type")
+        self.assertEqual(response._headers["content-type"][1], "text/piped; charset=utf-8")

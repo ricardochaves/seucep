ca522a1af59144dfa81195e22bdb52a3228f1422
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2019-10-29 22:20:14 -0300

    added startdata command

--- seucep/apps/core/models.py
+++ seucep/apps/core/models.py
@@ -8,7 +8,9 @@ class Addresses(models.Model):
     state = models.CharField(max_length=2, verbose_name="State")
     complement = models.CharField(blank=True, null=True, max_length=100, verbose_name="Complement")
     neighborhood = models.CharField(blank=True, null=True, max_length=72, verbose_name="Bairro")
-    city = models.CharField(max_length=72, verbose_name="Cidade")
+    city = models.CharField(blank=True, null=True, max_length=72, verbose_name="Cidade")
+    loc_nu = models.CharField(blank=True, null=True, max_length=72, verbose_name="loc_nu")
+    bairro_nu_ini = models.CharField(blank=True, null=True, max_length=72, verbose_name="bairro_nu_ini")
 
     def __str__(self):
         return self.cep
